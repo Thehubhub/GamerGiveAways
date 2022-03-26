1cf4aa5063354f3a802172b59da20a026c799837
consultant <kanyqy@gmail.com>
2022-03-25 20:17:52 -0400

    PS4 done

---
+++ app/src/main/java/com/example/gamergiveawaysapp/MyWorkmanagerApp.kt
@@ -0,0 +1,11 @@
+package com.example.gamergiveawaysapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyWorkmanagerApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_workmanager_app)
+    }
+}
